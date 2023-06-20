bd7455025d248b32a486d20de967c726f6fef7c6
Gyujinny <ginny.yang@slimmer.ai>
2023-06-20 16:55:35 +0200

    adding user

---
+++ app/core/tests/test_models.py
@@ -0,0 +1,73 @@
+"""
+Tests for models.
+"""
+from datetime import date
+
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+
+from core import models
+
+
+def create_user(email='user@example.com', password='testpass123'):
+    """Create a return a new user."""
+    return get_user_model().objects.create_user(email, password)
+
+
+class ModelTests(TestCase):
+    """Test models."""
+
+    def test_create_user_with_email_successful(self):
+        """Test creating a user with an email is successful."""
+        email = 'test@example.com'
+        password = 'testpass123'
+        user = get_user_model().objects.create_user(
+            email=email,
+            password=password,
+        )
+
+        self.assertEqual(user.email, email)
+        self.assertTrue(user.check_password(password))
+
+    def test_new_user_email_normalized(self):
+        """Test email is normalized for new users."""
+        sample_emails = [
+            ['test1@EXAMPLE.com', 'test1@example.com'],
+            ['Test2@Example.com', 'Test2@example.com'],
+            ['TEST3@EXAMPLE.com', 'TEST3@example.com'],
+            ['test4@example.COM', 'test4@example.com'],
+        ]
+        for email, expected in sample_emails:
+            user = get_user_model().objects.create_user(email, 'sample123')
+            self.assertEqual(user.email, expected)
+
+    def test_new_user_without_email_raises_error(self):
+        """Test that creating a user without an email raises a ValueError."""
+        with self.assertRaises(ValueError):
+            get_user_model().objects.create_user('', 'test123')
+
+    def test_create_superuser(self):
+        """Test creating a superuser."""
+        user = get_user_model().objects.create_superuser(
+            'test@example.com',
+            'test123',
+        )
+
+        self.assertTrue(user.is_superuser)
+        self.assertTrue(user.is_staff)
+
+    # def test_create_todoitem(self):
+    #     """Test creating a test_create_todo item is successful."""
+    #     user = get_user_model().objects.create_user(
+    #         'test@example.com',
+    #         'testpass123',
+    #     )
+    #     todo = models.TodoItem.objects.create(
+    #         user=user,
+    #         title='Sample Todo name',
+    #         duration_min=60,
+    #         due_date=date.today(),
+    #         description='Sample Todo description.',
+    #     )
+    #
+    #     self.assertEqual(str(todo), todo.title)
